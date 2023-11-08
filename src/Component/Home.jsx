5e21a9a9bab6b7a368e303c22eeb6be48c5fa127
Neelesh Kumar Yadav <yneelesh7388@gmail.com>
2023-11-09 01:35:14 +0530

    updated project with few changes

--- src/Component/Home.jsx
+++ src/Component/Home.jsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import {useNavigate} from 'react-router-dom';
-import LanguageTranslater from './LanguageTranslater';
-import TextForm from './TextForm';
 import { Link } from 'react-router-dom';
 
 export default function Home() {
-    const navigate=useNavigate();
+    
   return (
     <div className='home'>
       <img src={require('../photos/sunset-1401883.jpg')} alt="" />
